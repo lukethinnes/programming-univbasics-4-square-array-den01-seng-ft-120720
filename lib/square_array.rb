eae239d6265405cbe33f99bf27feb722ec06179b
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-18 21:33:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
-  counter = 0
-  while count<array.length {
-    array.index ** 2
+  new_numbers=array.new(array.size)
+  counter=0
+  while counter < array.size {
+    new_numbers[counter] = array[counter] ** 2
+    counter +=1
   }
-  square_array(array)
+  new_numbers
 end
