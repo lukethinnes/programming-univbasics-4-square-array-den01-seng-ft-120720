e228bf2107626d98645beab1c6aaf78c8459fb1c
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-18 21:50:22 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
-  # your code here
+  i = array.length - 1
+  while i > -1
+      array[i] = array[i] * array[i]
+      i = i - 1
+    end
+  array
+end
+
+
+#collect method
+def square_using_collect(array)
+  array.collect{|i| i**2}
 end
